059bba90b442e664b775af540fc1d384d1596432
novelyaasis32 <novelya32@gmail.com>
2022-09-15 22:37:39 +0700

    menambahkan view

--- projek_tsa/resources/views/mahasiswa/edit.blade.php
+++ projek_tsa/resources/views/mahasiswa/edit.blade.php
@@ -1,10 +1,10 @@
-@extends('pegawai.layout') 
+@extends('mahasiswa.layout') 
 @section('content') 
 <div class="container mt-5">
     <div class="row justify-content-center align-items-center"> 
         <div class="card" style="width: 24rem;"> 
             <div class="card-header text-center"> 
-                <h3>Edit Data Pegawai</h3>
+                <h3>Edit Data Mahasiswa</h3>
             </div>
             <div class="card-body"> @if ($errors->any()) 
                 <div class="alert alert-danger"> 
@@ -16,36 +16,28 @@
                     </ul> 
                 </div>
             @endif 
-            <form method="post" action="{{ route('pegawai.update', $Pegawai->id) }}" id="myForm"> 
+            <form method="post" action="{{ route('mahasiswa.update', $mahasiswa->id) }}" id="myForm"> 
             @csrf 
             @method('PUT')
                 <div class="form-group"> 
                     <label for="id">id</label> 
-                    <input type="text" name="id" class="form-control" id="id" value="{{ $Pegawai->id}}" aria-describedby="id" > 
+                    <input type="text" name="id" class="form-control" id="id" value="{{ $mahasiswa->id}}" aria-describedby="id" > 
                 </div>
                 <div class="form-group"> 
                     <label for="nama">Nama</label> 
-                    <input type="text" name="nama" class="form-control" id="nama" value="{{ $Pegawai->nama }}" aria-describedby="nama" > 
+                    <input type="text" name="nama" class="form-control" id="nama" value="{{ $mahasiswa->nama }}" aria-describedby="nama" > 
                 </div>
                 <div class="form-group"> 
-                    <label for="alamat">Alamat</label> 
-                    <input type="alamat" name="alamat" class="form-control" id="alamat" value="{{ $Pegawai->alamat }}" aria-describedby="alamat" > 
+                    <label for="kelas">Kelas</label> 
+                    <input type="kelas" name="kelas" class="form-control" id="alamat" value="{{ $mahasiswa->kelas }}" aria-describedby="kelas" > 
                 </div>
                 <div class="form-group"> 
-                    <label for="jabatan">Jabatan</label> 
-                    <input type="jabatan" name="jabatan" class="form-control" id="jabatan" value="{{ $Pegawai->jabatan }}" aria-describedby="jabatan" > 
-                </div>
-                <div class="form-group"> 
-                    <label for="divisi">Divisi</label>
-                        <select name="divisi" class="from-control">
-                            @foreach ($divisi as $dvs)
-                            <option value="{{$dvs->id}}" {{$Pegawai->divisi_id == $dvs->id ? 'selected' : '' }}>{{$dvs->nama_divisi}}</option>
-                        @endforeach 
-                    </select>
+                    <label for="jurusan">Jurusan</label> 
+                    <input type="jurusan" name="jurusan" class="form-control" id="jurusan" value="{{ $mahasiswa->jurusan }}" aria-describedby="jurusan" > 
                 </div>
                 <div class="d-grid gap-2 d-md-block">
                     <button type="submit" class="btn btn-primary">Submit</button>
-                    <a class="btn btn-success" href="{{ route('pegawai.index') }}">Back</a>
+                    <a class="btn btn-success" href="{{ route('mahasiswa.index') }}">Back</a>
                 </div> 
             </form> 
         </div> 
